b0a18f6f470ab9229909b5084e639a601ff2badc
JOOWON <ila_99@naver.com>
2023-05-21 21:00:43 +0900

    p40 함수 추출하기, 함수 인라인, 매개변수 삭제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,28 +8,34 @@ import java.text.NumberFormat;
 import java.util.*;
 
 public class Main {
+    private static Map<String, Play> plays;
     public static void main(String[]args) {
         ObjectMapper objectMapper = new ObjectMapper();
         List<Invoice> invoices;
-        Map<String, Play> plays;
+        //Map<String, Play> plays;  // 불변한 정적 맵으로 변경
 
         try {
-            invoices = objectMapper.readValue(new File("src/main/resources/json/invoices.json"), new TypeReference<List<Invoice>>(){});
-            plays = objectMapper.readValue(new File("src/main/resources/json/play.json"), new TypeReference<Map<String, Play>>(){});
+            invoices    = initializeInvoiceList(objectMapper);
+            plays       = initializePlayMap(objectMapper);
         } catch (IOException e) {
             System.out.println("[ERROR] IOException: " + e.getMessage());
             return;
         }
-        if (invoices == null || plays == null) {
-            System.out.println("[ERROR] Fail to read json..");
-            return;
-        }
+
         for (Invoice invoice : invoices) {
-            System.out.println(statement(invoice, plays));
+            System.out.println(statement(invoice));
         }
     }
 
-    public static String statement(Invoice invoice, Map<String, Play> plays) {
+    private static Map<String, Play> initializePlayMap(ObjectMapper objectMapper) throws IOException {
+        return objectMapper.readValue(new File("src/main/resources/json/play.json"), new TypeReference<Map<String, Play>>(){});
+    }
+
+    private static List<Invoice> initializeInvoiceList(ObjectMapper objectMapper) throws IOException {
+        return objectMapper.readValue(new File("src/main/resources/json/invoices.json"), new TypeReference<List<Invoice>>(){});
+    }
+
+    public static String statement(Invoice invoice) {   // 매개변수 삭제
         long totalAmount = 0;
         long volumeCredits = 0;
 
@@ -40,34 +46,14 @@ public class Main {
         numberFormat.setMinimumFractionDigits(2);
 
         for (Performance perf : invoice.getPerformances()) {
-            Play play = plays.get(perf.getPlayID());
-            int thisAmount = 0;
-
-            switch (play.getType()) {
-                case "tragedy": // 비극
-                    thisAmount = 40000;
-                    if(perf.getAudience() > 30) {
-                        thisAmount += 1000 * (perf.getAudience() - 30);
-                    }
-                    break;
-                case "comedy": // 희극
-                    thisAmount = 30000;
-                    if(perf.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-                    }
-                    thisAmount += 300 * perf.getAudience();
-                    break;
-                default:
-                    throw new Error(String.format("알 수 없는 장르: %s", play.getType()));
-            }
-
+            long thisAmount = amountFor(perf);        // 함수 추출, 인라인
             // 포인트를 적립한다.
             volumeCredits += Math.max(perf.getAudience() - 30, 0);
             // 희극 관객 5명마다 추가 포인트를 제공한다.
-            if (play.getType().equals("comedy")) volumeCredits += Math.floor(perf.getAudience() / 5);
+            if (playFor(perf).getType().equals("comedy")) volumeCredits += Math.floor(perf.getAudience() / 5);   // 인라인
 
             // 청구 내역을 출력한다.
-            result.append("    ").append(play.getName()).append(": ").append(numberFormat.format(thisAmount/100));
+            result.append("    ").append(playFor(perf).getName()).append(": ").append(numberFormat.format(thisAmount/100));  // 인라인
             result.append(" (").append(perf.getAudience()).append("석)\n");
             totalAmount += thisAmount;
         }
@@ -77,4 +63,32 @@ public class Main {
 
         return result.toString();
     }
+
+    
+    private static Play playFor(Performance perf) {
+        return plays.get(perf.getPlayID());
+    }
+
+    private static long amountFor(Performance aPerformance) {    // 매개변수 삭제
+        long result = 0;
+        switch (playFor(aPerformance).getType()) {      // 인라인
+            case "tragedy": // 비극
+                result = 40000;
+                if(aPerformance.getAudience() > 30) {
+                    result += 1000 * (aPerformance.getAudience() - 30);
+                }
+                break;
+            case "comedy": // 희극
+                result = 30000;
+                if(aPerformance.getAudience() > 20) {
+                    result += 10000 + 500 * (aPerformance.getAudience() - 20);
+                }
+                result += 300 * aPerformance.getAudience();
+                break;
+            default:
+                throw new Error(String.format("알 수 없는 장르: %s", playFor(aPerformance).getType()));
+        }
+        return result;
+    }
+
 }
