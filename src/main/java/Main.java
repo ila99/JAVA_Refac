c2b573457a0253e6dd1a0210fcf13e2a395552c6
JOOWON <ila_99@naver.com>
2023-05-21 21:28:03 +0900

    p50 - 반복문 쪼개기, 문장 슬라이드, 함수 추출, 변수 인라인

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 
@@ -37,36 +36,55 @@ public class Main {
 
     public static String statement(Invoice invoice) {   // 매개변수 삭제
         long totalAmount = 0;
-        long volumeCredits = 0;
 
         StringBuffer result = new StringBuffer("청구 내역 (고객명: \"");
         result.append(invoice.getCustomer()).append("\"\n");
 
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
-        numberFormat.setMinimumFractionDigits(2);
-
         for (Performance perf : invoice.getPerformances()) {
             long thisAmount = amountFor(perf);        // 함수 추출, 인라인
-            // 포인트를 적립한다.
-            volumeCredits += Math.max(perf.getAudience() - 30, 0);
-            // 희극 관객 5명마다 추가 포인트를 제공한다.
-            if (playFor(perf).getType().equals("comedy")) volumeCredits += Math.floor(perf.getAudience() / 5);   // 인라인
 
             // 청구 내역을 출력한다.
-            result.append("    ").append(playFor(perf).getName()).append(": ").append(numberFormat.format(thisAmount/100));  // 인라인
+            result.append("    ").append(playFor(perf).getName()).append(": ").append(usd(thisAmount));  // 인라인
             result.append(" (").append(perf.getAudience()).append("석)\n");
-            totalAmount += thisAmount;
+            totalAmount += thisAmount;    // totalAmount 누적 로직을 함수로 분리까지는 필요성이 잘..
         }
 
-        result.append("총액: ").append(numberFormat.format(totalAmount/100)).append("\n");
-        result.append("적립 포인트: ").append(volumeCredits).append("점\n");
+        result.append("총액: ").append(usd(totalAmount)).append("\n");
+        result.append("적립 포인트: ").append(totalVolumeCredits(invoice)).append("점\n");        // 인라인
 
         return result.toString();
     }
 
-    
-    private static Play playFor(Performance perf) {
-        return plays.get(perf.getPlayID());
+     // 값 누적 로직 분리 --> 만약 반복 횟수가 성능에 영향을 끼칠 정도이면 분리할 지 말 지는 상황에 맞게 고려해야 함
+    private static long totalVolumeCredits(Invoice invoice) {
+        long volumeCredits = 0;            // 문장 슬라이드 - 변수 초기화 문장을 for 문 앞으로
+        // 반복문 쪼개기
+        for (Performance perf : invoice.getPerformances()) {
+            volumeCredits += volumeCreditFor(perf);     // 함수 추출
+        }
+        return volumeCredits;
+    }
+
+    // usb로 변환
+    private static String usd(long aNumber) {
+        NumberFormat numFormat = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
+        numFormat.setMinimumFractionDigits(2);
+        return numFormat.format(aNumber/100);
+    }
+
+    private static long volumeCreditFor(Performance aPerformance) {
+        long result = 0;
+
+        // 포인트를 적립한다.
+        result += Math.max(aPerformance.getAudience() - 30, 0);
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if (playFor(aPerformance).getType().equals("comedy")) result += Math.floor(aPerformance.getAudience() / 5);   // 인라인
+        return result;
+    }
+
+
+    private static Play playFor(Performance aPerformance) {
+        return plays.get(aPerformance.getPlayID());
     }
 
     private static long amountFor(Performance aPerformance) {    // 매개변수 삭제
